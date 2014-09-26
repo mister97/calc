85e9894434711493d6dc2c7f06c1852d50a7db31
Kevin CRANE <kevin.craneg@gmail.com>
2014-09-26 09:56:15 -0400

    modif div.c

--- div.c
+++ div.c
@@ -1,6 +1,21 @@
 #include'calcul.h'
 
-float div(float a, float b)
+void div()
 {
-return (a/b);
+float resultat=0,a=0,b=0;
+
+printf("\nEntrez les valeurs de la division, a= ");
+scanf("%f",&a);
+printf("\nb= ");
+scanf("%f",&b);
+
+while(b==0)
+{
+printf("\nb ne peut pas etre egal a 0\n");
+printf("b= ");
+scanf("%f",&b);
+}
+
+resultat=a/b;
+printf("\nLe résultat est %f",resultat);
 }
